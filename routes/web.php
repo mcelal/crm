2422ca23199e4a18bab60c14a91e6babdd576bb2
Mehmet Celal Kara <info@mcelal.com>
2023-03-07 16:32:45 +0300

    ping endpoint

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ping', function () {
+    return response()->json([
+        'status' => 'success',
+    ]);
+});

bb0825e3aa445f8d223719270cb9ab5a6668a0ff
Mehmet Celal Kara <info@mcelal.com>
2023-03-21 09:27:06 +0300

    Install tenancy and laradumps

---
+++ routes/tenant.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
+
+Route::group([
+    'prefix'     => '/{tenant}',
+    'middleware' => [
+        'web',
+        Stancl\Tenancy\Middleware\InitializeTenancyByPath::class,
+//        PreventAccessFromCentralDomains::class,
+    ]
+], function () {
+    Route::get('/', function () {
+        return 'Tenant <b>' . tenant('id') . '</b>';
+    });
+});

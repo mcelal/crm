603732c0d73bd184094db6fe8a2d7521a9453922
Mehmet Celal Kara <info@mcelal.com>
2023-03-30 21:25:26 +0300

    Tenant create and edit before redirect to list page

--- app/Filament/Resources/TenantResource/Pages/CreateTenant.php
+++ app/Filament/Resources/TenantResource/Pages/CreateTenant.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTenant extends CreateRecord
 {
     protected static string $resource = TenantResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/TenantResource/Pages/EditTenant.php
+++ app/Filament/Resources/TenantResource/Pages/EditTenant.php
@@ -16,4 +16,9 @@ class EditTenant extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return self::$resource::getUrl('index');
+    }
 }

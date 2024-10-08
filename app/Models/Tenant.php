62c3871245a75fd36ed66df98ac0a8db990b3bd3
Mehmet Celal Kara <mehmet.celal.kara@macellan.net>
2024-10-08 23:42:52 +0300

    tenancy package installed

---
+++ app/Models/Tenant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase;
+    use HasDomains;
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+}
